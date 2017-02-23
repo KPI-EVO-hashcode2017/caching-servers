616df9aee62f7668153fee0ac589b462b0046639
vsel <volodymyr.volodymyrovych@gmail.com>
2017-02-23 21:15:16 +0200

    Remove kittens

--- parser.py
+++ parser.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 import numpy as np
 
 
@@ -40,4 +39,4 @@ def readfile(filename):
 
 
 from pprint import pprint
-pprint(readfile('data/kittens.in'))
+pprint(readfile('data/me_at_the_zoo.in'))
